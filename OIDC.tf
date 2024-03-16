c5b7f6771aa549b2871136e3113c7ff6c105fa34
Dtaylor07 <81515783+Dtaylor07@users.noreply.github.com>
2024-03-15 20:36:35 -0700

    Update OIDC.tf

--- OIDC.tf
+++ OIDC.tf
@@ -6,9 +6,9 @@ locals {
     "self"
   ]
 
-  oidc_sub_repositories = formatlist("Dtaylor07:org/%s:environment:%s", local.github_repositories, upper(var.environment))
+  oidc_sub_repositories = formatlist("repo:Dtaylor07:org/%s:environment:%s", local.github_repositories, upper(var.environment))
 
-  additional_permission = var.environment == "PROD" ? [format("repo:Dtaylor07/%s:environment:%s", local.github_repositories, "RELEASE")] : []
+  additional_permission = var.environment == "PROD" ? [formatlist("repo:Dtaylor07/%s:environment:%s", local.github_repositories, "RELEASE")] : []
 }
 
 # create github oidc provider
