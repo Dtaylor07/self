be84aeefa3dc757bcde0f1f92b114450f66977b5
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-03-28 21:26:29 -0700

    module test

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "website_bucket_arn" {
   description = "ARN of the bucket"
   value       = module.s3_bucket_module.arn
 }
+
+output "website_bucket_arn" {
+  description = "ARN of the bucket"
+  value       = module.s3_bucket_module.aws_vpc.main.arn
+}
