6ff4d807ac46028704151607de5f3a7a114be2c0
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-03-28 21:01:13 -0700

    module test

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
 output "website_bucket_arn" {
   description = "ARN of the bucket"
-  value       = module.s3_bucket.arn
+  value       = module.s3_bucket_module.arn
+}
+
+output "bucket_name" {
+  description = "Name of the bucket"
+  value       = module.s3_bucket_module.name
 }
